c774417019b08fb257f92fa464afca34d584ef34
ArtMirror <leonard.vanduuren@gmail.com>
2021-12-19 16:18:57 +0200

    Add files via upload

---
+++ livewshybrid/sendemail.php
@@ -0,0 +1,23 @@
+<?php
+         $to = $_POST['emailteacher'];
+         $subject = "Score for sheet: ".$_POST['sheetname'];
+         
+         $message = "<b>Score of: ".$_POST['namestudent']." for Sheet: ".$_POST['sheetname']." was: ".$_POST['score']."</b>";
+         //$message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:info@riadaamericanschool.org \r\n";
+         $header .= "Cc:".$_POST['emailstudent']." \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+          //  echo "Message sent successfully...";
+            header("location: ./endofsheet.html");
+
+         }else {
+          //  echo "Message could not be sent...";
+                        header("location: ./endofsheet.html");
+         }
+      ?>
